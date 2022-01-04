a09cb072e53fc880daf1c454516b2a1998b84cf7
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-04 09:30:13 +0100

    Started lesson 5

--- http.js
+++ http.js
@@ -1,17 +1,50 @@
 const { createServer } = require("http");
-const { createReadStream, stat } = require("fs");
+const { createReadStream, stat, createWriteStream } = require("fs");
 const { promisify } = require("util");
 
 const fileNmae = "./video.mp4";
 const fileInfo = promisify(stat);
 
-createServer(async (req, res) => {
+const sendOGVideo = async (req, res) => {
   const { size } = await fileInfo(fileNmae);
+  const { range } = req.headers;
 
-  res.writeHead(200, {
-    "Content-Length": size,
-    "Content-Type": "video/mp4",
-  });
+  if (range) {
+    let [start, end] = range.replace(/bytes=/, "").split("-");
+    start = +start;
+    end = end ? +end : size - 1;
 
-  createReadStream(fileNmae).pipe(res);
+    res.writeHead(206, {
+      "Content-Range": `bytes ${start}-${end}/${size}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": end - start + 1,
+      "Content-Type": "video/mp4",
+    });
+
+    createReadStream(fileNmae, { start, end }).pipe(res);
+  } else {
+    res.writeHead(200, {
+      "Content-Length": size,
+      "Content-Type": "video/mp4",
+    });
+
+    createReadStream(fileNmae).pipe(res);
+  }
+};
+
+createServer(async (req, res) => {
+  if (req.method === "POST") {
+    req.pipe(res);
+    req.pipe(process.stdout);
+    req.pipe(createWriteStream("./uopload.file"));
+  } else if (req.url === "/og") {
+    sendOGVideo(req, res);
+  } else {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <form enctype="multipart/form-data" method="POST" action="/">
+        <input type="file" name="upload-file">
+        <button>Upload File</button>
+      </form>`);
+  }
 }).listen(3000, () => console.log("Server running on port 3000"));
