8ec0c1d18e2cf46270e8ad1dc4663fb14de9d84e
Mario Lazzari <mario.lazzari@gmail.com>
2022-01-04 17:46:12 +0100

    Done

--- http.js
+++ http.js
@@ -1,42 +1,48 @@
 const { createServer } = require("http");
-const { createReadStream, stat, createWriteStream } = require("fs");
+const { stat, createReadStream, createWriteStream } = require("fs");
 const { promisify } = require("util");
+const multiparty = require("multiparty");
 
-const fileNmae = "./video.mp4";
+const filename = "./anime_dancing.mp4";
 const fileInfo = promisify(stat);
 
 const sendOGVideo = async (req, res) => {
-  const { size } = await fileInfo(fileNmae);
-  const { range } = req.headers;
-
+  const { size } = await fileInfo(filename);
+  const range = req.headers.range;
   if (range) {
     let [start, end] = range.replace(/bytes=/, "").split("-");
-    start = +start;
-    end = end ? +end : size - 1;
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
 
     res.writeHead(206, {
       "Content-Range": `bytes ${start}-${end}/${size}`,
       "Accept-Ranges": "bytes",
-      "Content-Length": end - start + 1,
+      "Content-Length": start - end + 1,
       "Content-Type": "video/mp4",
     });
 
-    createReadStream(fileNmae, { start, end }).pipe(res);
+    createReadStream(filename, { start, end }).pipe(res);
   } else {
     res.writeHead(200, {
       "Content-Length": size,
       "Content-Type": "video/mp4",
     });
-
-    createReadStream(fileNmae).pipe(res);
+    createReadStream(filename).pipe(res);
   }
 };
 
-createServer(async (req, res) => {
+createServer((req, res) => {
   if (req.method === "POST") {
-    req.pipe(res);
-    req.pipe(process.stdout);
-    req.pipe(createWriteStream("./uopload.file"));
+    let form = new multiparty.Form();
+    form.on("part", part => {
+      part
+        .pipe(createWriteStream(`./copy/${part.filename}`))
+        .on("close", () => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(`<h1>File Uploaded: ${part.filename}</h1>`);
+        });
+    });
+    form.parse(req);
   } else if (req.url === "/og") {
     sendOGVideo(req, res);
   } else {
@@ -45,6 +51,7 @@ createServer(async (req, res) => {
       <form enctype="multipart/form-data" method="POST" action="/">
         <input type="file" name="upload-file">
         <button>Upload File</button>
-      </form>`);
+      </form>
+    `);
   }
-}).listen(3000, () => console.log("Server running on port 3000"));
+}).listen(5050, () => console.log("server running on port 5050"));
